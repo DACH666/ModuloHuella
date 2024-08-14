34dab037d077c4e71ad336ced9bc21bd33d29d78
DACH <david_alejandro_956@hotmail.com>
2024-08-14 15:46:50 -0500

    Agregar archivos de proyecto.

---
+++ ModuloHuella/Cliente.cs
@@ -0,0 +1,20 @@
+﻿namespace ModuloHuella
+{
+    public class Cliente
+    {
+       public int IdCliente { get; set; }
+
+       public string Nombre { get; set; }
+
+       public string ApellidoP {  get; set; }
+
+        public string ApellidoM {  get; set; }
+
+        public int Celular {  get; set; }
+
+       
+        
+
+
+    }
+}
---
+++ ModuloHuella/ConexionDb.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ModuloHuella
+{
+    public class ConexionDb : DbContext
+    {
+        public ConexionDb(DbContextOptions<ConexionDb> options) 
+            : base(options)
+        {
+        }
+        
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ModuloHuella.db");
+        }
+
+    }
+}
