f01eafbdb7749ca587f5936bc6cc321769a9058b
bridgebed <omar.puentee1@gmail.com>
2024-08-23 19:48:33 -0500

    Integre side nv bar

---
+++ InterfazUsuario/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace InterfazUsuario
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
